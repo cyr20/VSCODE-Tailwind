dcdf2697e3ba713c15bfcfabdbd9d7586ccd812f
cyr20 <cyrelljoyvertudes@gmail.com>
2023-07-18 15:45:08 +0800

    responsive

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+var slider = tns({
+  container: ".slider__content",
+  items: 3,
+  slideBy: "page",
+  mouseDrag: true,
+  swipeAngle: false,
+  speed: 400,
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "0px",
+      lsm: "650px",
+      lmd: "770px",
+      md: "990px",
+      lg: "1200px",
+    },
     extend: {},
   },
   plugins: [],
